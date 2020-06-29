f8d857371ade80792ad7509f55bc0a7ef4b319dc
charlesonunze <charlesonunze2@gmail.com>
2020-06-29 01:32:38 +0100

    feat: 🎸 fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import axios from 'axios'
 import "./App.css";
 
 class App extends Component {
   state = {
     posts: []
   };
+
+  async componentDidMount() {
+    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    this.setState({ posts })
+  }
 
   handleAdd = () => {
     console.log("Add");
@@ -21,7 +27,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <button className="btn btn-primary" onClick={this.handleAdd}>
+        <button className="btn btn-primary" onClick={ this.handleAdd }>
           Add
         </button>
         <table className="table">
@@ -33,13 +39,13 @@ class App extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.posts.map(post => (
-              <tr key={post.id}>
-                <td>{post.title}</td>
+            { this.state.posts.map(post => (
+              <tr key={ post.id }>
+                <td>{ post.title }</td>
                 <td>
                   <button
                     className="btn btn-info btn-sm"
-                    onClick={() => this.handleUpdate(post)}
+                    onClick={ () => this.handleUpdate(post) }
                   >
                     Update
                   </button>
@@ -47,13 +53,13 @@ class App extends Component {
                 <td>
                   <button
                     className="btn btn-danger btn-sm"
-                    onClick={() => this.handleDelete(post)}
+                    onClick={ () => this.handleDelete(post) }
                   >
                     Delete
                   </button>
                 </td>
               </tr>
-            ))}
+            )) }
           </tbody>
         </table>
       </React.Fragment>
